ceb045c897fe8192e4797b34f4c0c18206a315a8
Vivek Saurabh <vivek.saurabh@axi.com>
2025-02-05 03:16:59 +0530

    adding stuff back again

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,15 +4,15 @@ import './index.css'
 import App from './App.tsx'
 import { ApolloProvider } from '@apollo/client';
 import client from './common/apolloClient';
-// import { msalInstance } from './config/msal';
-// import { MsalProvider } from '@azure/msal-react';
+import { msalInstance } from './config/msal';
+import { MsalProvider } from '@azure/msal-react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ApolloProvider client={client}>
-      {/* <MsalProvider instance={msalInstance}> */}
-      <App />
-      {/* </MsalProvider> */}
+      <MsalProvider instance={msalInstance}>
+        <App />
+      </MsalProvider>
     </ApolloProvider>
   </StrictMode>,
 )
