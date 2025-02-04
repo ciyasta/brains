d43b699d86984b3884fc38f491eca69f95e05883
Vivek Saurabh <vivek.saurabh@axi.com>
2025-02-05 04:28:24 +0530

    Revert "adding token to request header"
    This reverts commit 66bc4499ee50c083bbc14cd12939a82efefd338c.

--- frontend/src/common/apolloClient.ts
+++ frontend/src/common/apolloClient.ts
@@ -1,30 +1,8 @@
 // apolloClient.js
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { useMsal } from '@azure/msal-react';
-
-const httpLink = createHttpLink({
-    uri: 'https://yoga-server.thankfulbush-f35cdce5.southindia.azurecontainerapps.io/graphql',
-});
-
-const authLink = setContext(async (_, { headers }) => {
-    const { instance, accounts } = useMsal();
-    await instance.initialize();
-    const tokenResponse = await instance.acquireTokenSilent({
-        scopes: ["api://1def8333-5f90-4bd2-ba69-294b0a52e80a/user_impersonation"],
-        account: accounts[0]
-    });
-    console.log(tokenResponse);
-    return {
-        headers: {
-            ...headers,
-            authorization: `Bearer ${tokenResponse.accessToken}`,
-        }
-    }
-});
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    uri: 'https://yoga-server.thankfulbush-f35cdce5.southindia.azurecontainerapps.io/graphql', // Replace with your GraphQL endpoint
     cache: new InMemoryCache(),
 });
 
