c8c0bb2e1a7f333dce9a09434e5e0b8e1c7aa064
Vivek Saurabh <vivek.saurabh@axi.com>
2025-02-05 06:56:45 +0530

    changing to api app

--- frontend/src/config/msal.ts
+++ frontend/src/config/msal.ts
@@ -2,7 +2,7 @@ import { PublicClientApplication } from "@azure/msal-browser";
 
 const msalConfig = {
     auth: {
-        clientId: "bee9a708-d78b-4cc5-a5b0-86bb64446e4d", // Entra ID App ID for React
+        clientId: "1def8333-5f90-4bd2-ba69-294b0a52e80a", // Entra ID App ID for React
         authority: "https://login.microsoftonline.com/1958953c-c8e8-4b91-986b-718a64ae3b81",
         redirectUri: "https://green-beach-0dbcca000.4.azurestaticapps.net/",
     },
